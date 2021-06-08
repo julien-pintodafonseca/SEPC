f468ccbab2360e0a73bdd3bb34fb3695c0c2f5f1
Léa Barrau <lea.barrau1@ensimag.grenoble-inp.fr>
2021-06-08 09:15:56 +0200

    Phase 1 : Printf OK

--- kernel/start.c
+++ kernel/start.c
@@ -1,5 +1,6 @@
 #include "debugger.h"
 #include "cpu.h"
+#include "printf.c"
 
 int fact(int n)
 {
@@ -13,13 +14,16 @@ int fact(int n)
 void kernel_start(void)
 {
 	int i;
-	// call_debugger(); useless with qemu -s -S
+	// call_debugger(); // useless with qemu -s -S
 
 	i = 10;
 
 	i = fact(i);
+    //for (int j = 0 ; j < 40 ; j++) {
+    //    printf("abcde");
+    //}
 
-	while(1)
+    while(1)
 	  hlt();
 
 	return;
